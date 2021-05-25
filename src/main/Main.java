45f1968cf1158ee81fd3245107a8afc89939725c
Miguel Costa <fc54431@alunos.fc.ul.pt>
2021-05-25 23:28:36 +0100

    Criado Esqueleto e Iniciado Main

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao teu Contador de Roupa! Vamos Começar :)");
+
+        System.out.println("Estamos a trabalhar com quantas variáveis?");
+        int apagar = sc.nextInt();
+
+
+    }
+}
